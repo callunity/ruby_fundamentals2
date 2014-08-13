3560e5c9e8468de6dca463057d9462be74d64fe8
callunity <heather.e.armstrong@gmail.com>
2014-08-13 14:55:27 -0400

    Exercise 6 steps 2 and 3

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,21 @@
 grocery_list = ["peanut butter", "yogurt", "onions", "red wine vinegar"]
 
+
+# 1.
 grocery_list.each do |item|
 	puts "* #{item}"
 end
+
+puts
+# 2.
+grocery_list << "rice"
+grocery_list.each do |item|
+	puts "* #{item}"
+end
+
+puts
+#3. 
+def glist_print(glist)
+	glist.each { |item| puts "* #{item}"}
+end
+glist_print(grocery_list)
