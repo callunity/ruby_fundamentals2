1e923d67f63bdd8608161f7784a56432ce408c19
callunity <heather.e.armstrong@gmail.com>
2014-08-13 15:16:10 -0400

    Exercise 2 completed

--- exercise6.rb
+++ exercise6.rb
@@ -6,27 +6,43 @@ grocery_list.each do |item|
 	puts "* #{item}"
 end
 
-puts
+
 # 2.
 grocery_list << "rice"
 grocery_list.each do |item|
 	puts "* #{item}"
 end
 
-puts
-#3 . 
+
+# 2.1 
 def glist_print(glist)
 	glist.each { |item| puts "* #{item}"}
 end
 glist_print(grocery_list)
 
-puts
-# 4.
+
+# 3.
 puts grocery_list.length
 
-puts
-# 5.
+
+# 4.
 if grocery_list.include? "bananas" == true
 	puts "You need to pick up bananas."
 else puts "You don't need to pick up bananas."
 end
+
+
+# 5.
+puts grocery_list[1]
+
+
+# 6.
+grocery_list.sort! { |a,b| a <=> b }
+glist_print(grocery_list)
+
+# 7.
+grocery_list << "salmon"
+glist_print(grocery_list)
+puts "Removing salmon"
+grocery_list.delete("salmon")
+glist_print(grocery_list)
