273e1d8cc36d2b2d26a1ba91037bc29a409be4fa
callunity <heather.e.armstrong@gmail.com>
2014-08-13 17:37:21 -0400

    Exercise 7 #6

--- exercise6.rb
+++ exercise6.rb
@@ -1,48 +1,49 @@
 grocery_list = ["peanut butter", "yogurt", "onions", "red wine vinegar"]
 
 
-# 1.
+# 1. Display grocery list according to specified formatting
 grocery_list.each do |item|
 	puts "* #{item}"
 end
 
 
-# 2.
+# 2. Add "rice" to grocery list, then display list
 grocery_list << "rice"
 grocery_list.each do |item|
 	puts "* #{item}"
 end
 
 
-# 2.1 
+# 2.1 Write function to display grocery list according to specs
 def glist_print(glist)
 	glist.each { |item| puts "* #{item}"}
 end
 glist_print(grocery_list)
 
 
-# 3.
+# 3. Display how many items are on the grocery list
 puts grocery_list.length
 
 
-# 4.
+# 4. Determine whether bananas need to be bought
 if grocery_list.include? "bananas" == true
 	puts "You need to pick up bananas."
 else puts "You don't need to pick up bananas."
 end
 
 
-# 5.
+# 5. Display the second item on the grocery list
 puts grocery_list[1]
 
 
-# 6.
+# 6. Sort grocery list alphabetically
 grocery_list.sort! { |a,b| a <=> b }
 glist_print(grocery_list)
 
-# 7.
+# 7. (Add salmon to grocery list, then) Delete salmon from list
 grocery_list << "salmon"
 glist_print(grocery_list)
 puts "Removing salmon"
 grocery_list.delete("salmon")
 glist_print(grocery_list)
+
--- exercise7.rb
+++ exercise7.rb
@@ -4,28 +4,35 @@ students = {
 	:cohort3 => 22
 }
 
-# 1.
+# 1. Display each cohort name and its number of students
 def cohort_display(cohort)
 	cohort.each { |cohort, students| puts "#{cohort}: #{students} students" }
 end
 
 cohort_display(students)
 
-# 2.
+# 2. Add cohort 4
 students[:cohort4] = 43
 
-# 3.
+# 3. Output all cohort names
 puts students.keys
 
-# 4.
+# 4. Increase each cohort by 5% then display the results
 students.each do |cohort, student_num| 
 	students[cohort] = (student_num * 1.05).to_i
 end
 
 cohort_display(students)
 
-# 5. 
+# 5. Delete second cohort, then display all cohorts
 students.delete(:cohort2)
 cohort_display(students)
 
-# 6.
+# 6. Calculate the total number of students in all cohorts
+student_sum = 0
+students.each do |cohort, students| 
+	student_sum += students
+end
+
+puts student_sum
+
